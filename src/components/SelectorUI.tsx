a36c85d8d5ee2e18b1233fd50ff9183970af21aa
Levin514 <152341877+Levin514@users.noreply.github.com>
2025-06-25 14:52:34 +0000

    Guía 14 completada

--- src/components/SelectorUI.tsx
+++ src/components/SelectorUI.tsx
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
-import Select from '@mui/material/Select';
+import Select, { type SelectChangeEvent } from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 
 export default function SelectorUI() {
+   const [cityInput, setCityInput] = useState('');
+
+   const handleChange = (event: SelectChangeEvent<string>) => {
+    setCityInput(event.target.value);
+};
 
 return (
    <FormControl fullWidth>
@@ -11,14 +17,24 @@ return (
       <Select
          labelId="city-select-label"
          id="city-simple-select"
-         label="Ciudad">
+         label="Ciudad"
+         value={cityInput}
+         onChange={handleChange} >
          <MenuItem disabled><em>Seleccione una ciudad</em></MenuItem>
          <MenuItem value={"guayaquil"}>Guayaquil</MenuItem>
          <MenuItem value={"quito"}>Quito</MenuItem>
          <MenuItem value={"manta"}>Manta</MenuItem>
          <MenuItem value={"cuenca"}>Cuenca</MenuItem>
       </Select>
-
+      {cityInput && (
+         <p>
+            Información del clima en{' '}
+            <b>
+              {cityInput.charAt(0).toUpperCase() + cityInput.slice(1)}
+            </b>
+            .
+         </p>
+      )}
    </FormControl>
    )
 }
