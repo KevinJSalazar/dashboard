0ff71eb3d5a04f967cb72cb38a172dbfb98e3fad
Levin514 <152341877+Levin514@users.noreply.github.com>
2025-07-07 15:44:26 +0000

    Guía 15 completa

---
+++ src/functions/DataFetcher.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import type { OpenMeteoResponse } from "../types/DashboardTypes";
+
+const DataFetcher = () => {
+  const [data, setData] = useState<OpenMeteoResponse | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const url =
+      "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current=temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m&timezone=America%2FChicago";
+      
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Error en la petición: ${response.status}`);
+        }
+        const result: OpenMeteoResponse = await response.json();
+        setData(result);
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+};
+
+export default DataFetcher;
